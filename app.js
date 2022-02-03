8846c7830bcc36f4c1f6496df889d1da0c92d683
Mihir Harshe <mihir.harshe12@gmail.com>
2022-02-04 01:44:43 +0530

    few changes

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 var app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
